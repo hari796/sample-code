caec070d859aa9795813aa83ef5537435ee7f7f2
S Hari Prasad <55071639+hari796@users.noreply.github.com>
2022-04-05 15:48:47 +0530

    Update install_dependencies01.sh

--- script/install_dependencies01.sh
+++ script/install_dependencies01.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #apt-get install apache2 -y
-cp -rf /tmp/index.html /var/www/html/index.html
-rm -rf /tmp/index.html
+rm -rf /var/www/html/index.html
+cp -rf /opt/r1/a/_hari796_sample-code/script/index.html /var/www/html/
+#rm -rf /tmp/index.html
