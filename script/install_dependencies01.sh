3f2b7a379431f8ddcc3a96c5a4afbf63da4844f8
S Hari Prasad <55071639+hari796@users.noreply.github.com>
2022-06-14 17:45:28 +0530

    Update install_dependencies01.sh

--- script/install_dependencies01.sh
+++ script/install_dependencies01.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #apt-get install apache2 -y
 sudo rm -rf /var/www/html/index.html
-sudo cp -rf /tmp/script/index.html /var/www/html/
+sudo cp -rf /temp/script/index.html /var/www/html/
 #rm -rf /tmp/index.html
